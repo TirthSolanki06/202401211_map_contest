a422bc680e54625a35cca4d8b901a428a9b04d38
TirthSolanki06 <nitiram2506@gmail.com>
2025-04-04 12:25:10 +0530

    Update first_unique_character.cpp

---
+++ first_unique_character.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int firstUniqChar(string s) {
+    unordered_map<char, int> count;
+    for (char c : s){ 
+        count[c]++;
+    }
+    for (int i = 0; i < s.size(); ++i){
+        if (count[s[i]] == 1) return i;
+    }
+    return -1;
+}
+
+int main() {
+    string s = "leetcode";
+    cout << firstUniqChar(s) << endl; 
+    return 0;
+}
